2501fc9dfa894ccbcaee7e51cca6a1a54b7beb61
Danil Zharkov <zharkov.danik.ua@gmail.com>
2025-07-01 21:34:16 -0600

    Navigation(Last version of mini app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
-import Cart from './pages/CartPage/Cart';
-import Home from './pages/HomePage/Home';
+import { Routes, Route, NavLink } from 'react-router-dom'
+import Home from './pages/HomePage/Home'
+import Cart from './pages/CartPage/Cart'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <div className='app-container'>
-      <Home />
-      <Cart /> 
-      </div>
-      
+    <div>
+      <nav className="nav-bar">
+        <NavLink to="/" className={'nav-link'}>Home</NavLink> | <NavLink to="/cart" className={'nav-link'}>Cart</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux'; 
-import store from './redux/store';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
-);
+)
